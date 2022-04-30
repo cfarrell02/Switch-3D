83e32be59a98f37b6380617568562df5fe0000f1
cfarrell02 <cianster24@gmail.com>
2022-04-30 15:36:43 +0100

    Initial Commit

---
+++ CA 4 Game/Assets/Scripts/MouseLook.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class MouseLook : MonoBehaviour
+{
+    public Transform playerBody;
+    Vector2 lookDirection;
+    public float lookSpeed = 100f, xRotation = 0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        lookSpeed /= 1000;
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Look();
+        playerBody.Rotate(Vector3.up * lookDirection.x *lookSpeed );
+        
+    }
+    public Vector2 getLookDirection()
+    {
+        return lookDirection;
+    }
+    void OnLook(InputValue input)
+    {
+        lookDirection = input.Get<Vector2>();
+    }
+    private void Look()
+    {
+        xRotation -= lookDirection.y * lookSpeed;
+     //   xRotation *= lookSpeed;
+        xRotation = Mathf.Clamp(xRotation, -90f, 60f);
+        transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
+    }
+}
---
+++ CA 4 Game/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,187 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.SceneManagement;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public CharacterController controller;
+    Vector2 moveDirection;
+    Vector3 velocity;
+    public float speed = 15f, gravity = -9.8f, groundDistance = 2f, jumpHeight = 3f, sprintModifier = 1.5f;
+    public Transform groundCheck;
+    public LayerMask groundMask;
+    private bool isGrounded, isSprinting, isSwitched, isAtButton, physicsObject, holdingObject;
+    public Animator animator, uiAnimator;
+    Animator buttonAnimator;
+    AudioSource audioSource;
+    public AudioClip[] footsteps;
+    public bool isPaused = false;
+    public GameObject camera;
+    GameObject physicsItem;
+    CharacterController characterController;
+
+    void Start()
+    {
+        holdingObject = false;
+        characterController = GetComponent<CharacterController>();
+        isSwitched = false;
+        audioSource = GetComponent<AudioSource>();
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        if (!isPaused)
+        {
+            Move();
+            Sound();
+        }
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.layer == 4)
+        {
+            StartCoroutine( Die());
+        }else if(other.gameObject.layer == 7)
+        {
+            isAtButton = true;
+            buttonAnimator = other.gameObject.GetComponent<Animator>();
+        }else if(other.gameObject.layer == 8)
+        {
+            physicsObject = true;
+            physicsItem = other.gameObject;
+        }
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.layer == 7)
+            isAtButton = false;
+        else if(other.gameObject.layer == 8)
+            physicsObject = false;
+    }
+
+    IEnumerator Die()
+    {
+        yield return new WaitForSeconds(1);
+        isPaused = true;
+        uiAnimator.SetBool("isDead", true);
+        StartCoroutine(returnToMenu());
+    }
+
+    IEnumerator returnToMenu()
+    {
+        yield return new WaitForSeconds(5);
+        Cursor.lockState = CursorLockMode.None;
+        SceneManager.LoadScene("Menu");
+
+    }
+
+
+    void Sound()
+    {
+        int index = Random.Range(0, footsteps.Length - 1);
+        if (moveDirection.magnitude > 0 && isGrounded && !audioSource.isPlaying)
+        {
+            audioSource.PlayOneShot(footsteps[index]);
+        }
+
+    }
+
+
+    void OnUse(InputValue input)
+    {
+        print("Is Near Physics Object " + physicsObject + "\n Is Holding it? " + holdingObject);
+        if (isAtButton && !uiAnimator.GetBool("teleport"))
+            StartCoroutine(teleport());
+        else if (physicsObject && !holdingObject)
+        {
+
+
+            holdingObject = true;
+            physicsItem.transform.SetParent(this.transform);
+            physicsItem.GetComponent<Rigidbody>().useGravity = false;
+            physicsItem.GetComponent<Rigidbody>().isKinematic = true;
+            physicsItem.transform.position = camera.transform.position + camera.transform.forward * 3;
+        }
+        else if (holdingObject)
+        {
+
+            holdingObject = false;
+            physicsItem.GetComponent<Rigidbody>().useGravity = true;
+            physicsItem.GetComponent<Rigidbody>().isKinematic = false;
+            physicsItem.transform.SetParent(null);
+        
+        }
+    }
+
+    IEnumerator teleport()
+        {
+        buttonAnimator.SetBool("IsPressed", true);
+        isPaused = true;
+        uiAnimator.SetBool("teleport", true);
+        
+        yield return new WaitForSeconds(1.1f);
+        characterController.enabled = false;
+        if (!isSwitched)
+        {
+            isSwitched = true;
+            transform.position = new Vector3(transform.position.x - 200, transform.position.y, transform.position.z);
+        }
+        else
+        {
+            isSwitched = false;
+            transform.position = new Vector3(transform.position.x + 200, transform.position.y, transform.position.z);
+        }
+        characterController.enabled = true;
+        isPaused = false;
+        yield return new WaitForSeconds(5);
+        uiAnimator.SetBool("teleport", false);
+        buttonAnimator.SetBool("IsPressed", false);
+
+    }
+
+    void OnMove(InputValue input)
+    {
+        moveDirection = input.Get<Vector2>();
+       
+    }
+
+    void OnSprint(InputValue input)
+    {
+        if (input.isPressed)
+        {
+            isSprinting = true;
+        }else
+            isSprinting=false;
+    }
+
+    void OnJump(InputValue input)
+    {
+            
+            if (isGrounded) { 
+        velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
+           
+    }
+        
+        
+    }
+
+    private void Move()
+    {
+        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+        if(isGrounded && velocity.y < 0)
+        {
+            velocity.y = -2f;
+        }
+        Vector3 move = transform.right * moveDirection.x + transform.forward * moveDirection.y;
+
+        if(isSprinting)
+            controller.Move(move * (sprintModifier*speed) / 100);
+        else
+        controller.Move(move * speed / 100);
+        velocity.y += gravity * Time.deltaTime;
+        controller.Move(velocity * Time.deltaTime);
+    }
+}
